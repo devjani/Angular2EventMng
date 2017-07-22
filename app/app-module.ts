7a8ec847a5f9685a1a721f099b7572264d351b07
Dev <janidev94@gmail.com>
2017-07-22 11:13:07 +0530

    first commit

---
+++ app/app-module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {EventAppComponent} from './event-app.component'
+ @NgModule({
+    imports:[BrowserModule],
+    declarations:[EventAppComponent],
+    bootstrap:[EventAppComponent]
+ })
+ export class AppModule{
+
+ }
